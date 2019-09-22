03aab23c3acd8a17f08949727ed03ff0a51865f3
edf10 <eduarda.mirelly@academico.ifrn.edu.br>
2019-09-22 00:27:42 -0300

    A

--- src/jogodavelha/IntroductionJDV.java
+++ src/jogodavelha/IntroductionJDV.java
@@ -1,5 +1,19 @@
 package jogodavelha;
-import 
-public class IntroductionJDV {
+import padroes.IntroductionGame;
+public class IntroductionJDV extends IntroductionGame{
+
+    public IntroductionJDV(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void niveis() {
+    }
+    @Override
+    public void tutorial() {
+    }
+    @Override
+    public void intro() {
+    }
     
 }
