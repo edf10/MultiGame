62578b78c415caffd33565fd5d17f4ca9abfea8e
edf10 <eduarda.mirelly@academico.ifrn.edu.br>
2019-09-23 20:11:41 -0300

    Configurações

---
+++ src/campominado/Recordes.java
@@ -0,0 +1,24 @@
+package campominado;
+import componentes.Pn;
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import user.User;
+
+public class Recordes extends AbstractTableModel{
+
+    @Override
+    public int getRowCount() {
+        return 1;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        return null;
+    }
+    
+}
