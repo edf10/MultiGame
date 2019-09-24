669a20253d004ef0747c1281e1038a1c0eb5968a
edf10 <eduarda.mirelly@academico.ifrn.edu.br>
2019-09-24 02:38:47 -0300

    Criptografia

--- src/multigame/MultiGame.java
+++ src/multigame/MultiGame.java
@@ -1,12 +1,14 @@
 package multigame;
-import user.Cadastro;
+import java.util.Arrays;
+import user.Criptografar;
 import testes.Criptografia;
 public class MultiGame {
     public static void main(String []args) {
         //TelaGOWin t = new TelaGOWin("Jogador01");
-        //MultiGameTela mg = new MultiGameTela(1);
+        MultiGameTela mg = new MultiGameTela(1);
         //Cadastro c = new Cadastro();
-        Criptografia c = new Criptografia();
-        
+        //Criptografar c = new Criptografar("1234567891234567", "0801");
+        //System.out.println(Arrays.toString(c.encriptar()));
+        //System.out.println(c.descriptar());
     }
 }
--- src/user/Criptografar.java
+++ src/user/Criptografar.java
@@ -1,4 +1,42 @@
 package user;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Criptografar {
+    
+    private String chave;
+    private String texto;
+    private SecretKey key;
+    private byte[] tencrip;
+    private byte[] tdencrip;
+    private Cipher cp;
+    
+    public Criptografar(String chave, String texto){
+        this.chave = chave;
+        key = new SecretKeySpec(this.chave.getBytes(), "AES");
+        this.texto = texto;
+    }
+    
+    public byte[] encriptar(){
+        try{
+            cp = Cipher.getInstance("AES");
+            cp.init(Cipher.ENCRYPT_MODE, key);
+            tencrip = cp.doFinal(texto.getBytes());
+            return tencrip;
+        }catch(Exception e){}
+        return null;
+    }
+    
+    public String descriptar(){
+        try{
+            cp = Cipher.getInstance("AES");
+            cp.init(Cipher.DECRYPT_MODE, key);
+            tdencrip = cp.doFinal(tencrip);
+            texto = new String(tdencrip);
+            return texto;
+        }catch(Exception e){}
+        return null;
+    }
     
 }
