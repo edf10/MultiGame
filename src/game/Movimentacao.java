1449634701b8af2b647b5c8314398603f4cce2af
edf10 <50642233+edf10@users.noreply.github.com>
2019-09-27 11:17:18 -0300

    Movimentação

--- src/game/Movimentacao.java
+++ src/game/Movimentacao.java
@@ -1,6 +1,7 @@
 package game;
 import componentes.Frame;
 import componentes.Lb;
+import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
@@ -14,14 +15,19 @@ public class Movimentacao extends Frame{
     
     private int posX = 100;
     private int posY = 300;
+    private int imTroca = 0;
+    private Dimension d = new Dimension(800,600);
     
     public Movimentacao(){
         super(800,600);
         addMovimento();
+        add(lbdeath);
         setVisible(true);
     }
     
     public void addMovimento(){
+        int pDeath[] = {posX,posY,32,48};
+        lbdeath = new Lb(walkRight1, pDeath);
         addKeyListener(new KeyListener(){
             @Override
             public void keyTyped(KeyEvent e) {
@@ -31,20 +37,35 @@ public class Movimentacao extends Frame{
             public void keyPressed(KeyEvent e) {
                 //Up=38;Back=40;Left=37;Right=39
                 
-                switch(e.getKeyCode()){
-                    case 38:break;
-                    case 40:break;
-                    case 37:break;
-                    case 39:break;
+                switch(imTroca){
+                    case 0:lbdeath.setIcon(walkRight1);imTroca = 1;break;
+                    case 1:lbdeath.setIcon(walkRight2);imTroca = 2;break;
+                    case 2:lbdeath.setIcon(walkRight3);imTroca = 3;break;
+                    case 3:lbdeath.setIcon(walkRight4);imTroca = 0;break;
                     default:break;
                 }
+                System.out.println(d.height+" "+d.width);
                 
+                if(posX!=d.width-4||posY!=d.height){
+                    switch(e.getKeyCode()){
+                        case 38:posY-=4;break;
+                        case 40:posY+=4;break;
+                        case 37:posX-=4;break;
+                        case 39:posX+=4;break;
+                        default:break;
+                    }
+                }
+                System.out.println(posX+" "+posY);
+                atualizar();
                 System.out.println(e.getKeyCode());
             }
             @Override
             public void keyReleased(KeyEvent e) {
+                lbdeath.setIcon(walkRight1);
             }
         });
     }
-    
+    public void atualizar(){
+        lbdeath.setBounds(posX, posY, 32, 48);
+    }
 }
