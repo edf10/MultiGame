34f837a7752a8c1a50c712707585c263266ebc7c
edf10 <50642233+edf10@users.noreply.github.com>
2019-09-26 21:51:09 -0300

    KeyListener

--- src/game/Movimentacao.java
+++ src/game/Movimentacao.java
@@ -1,13 +1,14 @@
 package game;
+import componentes.Frame;
 import componentes.Lb;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
-public class Movimentacao{
-    private final ImageIcon walkRight1 = new ImageIcon(getClass().getResource("death_right_01.png"));
-    private final ImageIcon walkRight2 = new ImageIcon(getClass().getResource("death_right_02.png"));
-    private final ImageIcon walkRight3 = new ImageIcon(getClass().getResource("death_right_03.png"));
-    private final ImageIcon walkRight4 = new ImageIcon(getClass().getResource("death_right_04.png"));
+public class Movimentacao extends Frame{
+    private final ImageIcon walkRight1 = new ImageIcon(getClass().getResource("imagens/00/death_right_01.png"));
+    private final ImageIcon walkRight2 = new ImageIcon(getClass().getResource("imagens/00/death_right_02.png"));
+    private final ImageIcon walkRight3 = new ImageIcon(getClass().getResource("imagens/00/death_right_03.png"));
+    private final ImageIcon walkRight4 = new ImageIcon(getClass().getResource("imagens/00/death_right_04.png"));
     
     private Lb lbdeath;
     
@@ -15,8 +16,25 @@ public class Movimentacao{
     private int posY = 300;
     
     public Movimentacao(){
+        super(800,600);
+        addMovimento();
+        setVisible(true);
     }
     
-    
+    public void addMovimento(){
+        addKeyListener(new KeyListener(){
+            @Override
+            public void keyTyped(KeyEvent e) {
+                
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("OK");
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+    }
     
 }
--- src/multigame/MultiGame.java
+++ src/multigame/MultiGame.java
@@ -1,7 +1,10 @@
 package multigame;
+
+import game.Movimentacao;
+
 public class MultiGame {
     public static void main(String []args){
         //MultiGameTela mg = new MultiGameTela(1);
-        
+        Movimentacao m = new Movimentacao();
     }
 }
