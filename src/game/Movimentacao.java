08aa476d0ca263c2e92c420363e16de06dff79b6
edf10 <eduarda.mirelly@academico.ifrn.edu.br>
2019-09-28 00:23:12 -0300

    Testes

--- src/game/Movimentacao.java
+++ src/game/Movimentacao.java
@@ -1,10 +1,12 @@
 package game;
 import componentes.Frame;
 import componentes.Lb;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 public class Movimentacao extends Frame{
     private final ImageIcon walkRight1 = new ImageIcon(getClass().getResource("imagens/00/death_right_01.png"));
     private final ImageIcon walkRight2 = new ImageIcon(getClass().getResource("imagens/00/death_right_02.png"));
@@ -12,22 +14,29 @@ public class Movimentacao extends Frame{
     private final ImageIcon walkRight4 = new ImageIcon(getClass().getResource("imagens/00/death_right_04.png"));
     
     private Lb lbdeath;
+    private JLabel lbfundo;
     
-    private int posX = 100;
+    private int posX = 81;
     private int posY = 300;
     private int imTroca = 0;
     private Dimension d = new Dimension(800,600);
     
     public Movimentacao(){
         super(800,600);
+        lbfundo = new JLabel();
+        lbfundo.setBounds(0, 0, 800, 600);
+        lbfundo.setOpaque(true);
+        lbfundo.setBackground(Color.yellow);
+        int pDeath[] = {posX,posY,32,48};
+        lbdeath = new Lb(walkRight1, pDeath);
+        
         addMovimento();
         add(lbdeath);
+        add(lbfundo);
         setVisible(true);
     }
     
     public void addMovimento(){
-        int pDeath[] = {posX,posY,32,48};
-        lbdeath = new Lb(walkRight1, pDeath);
         addKeyListener(new KeyListener(){
             @Override
             public void keyTyped(KeyEvent e) {
@@ -36,7 +45,6 @@ public class Movimentacao extends Frame{
             @Override
             public void keyPressed(KeyEvent e) {
                 //Up=38;Back=40;Left=37;Right=39
-                
                 switch(imTroca){
                     case 0:lbdeath.setIcon(walkRight1);imTroca = 1;break;
                     case 1:lbdeath.setIcon(walkRight2);imTroca = 2;break;
@@ -44,9 +52,9 @@ public class Movimentacao extends Frame{
                     case 3:lbdeath.setIcon(walkRight4);imTroca = 0;break;
                     default:break;
                 }
-                System.out.println(d.height+" "+d.width);
-                
-                if(posX!=d.width-4||posY!=d.height){
+                //System.out.println(d.height+" "+d.width);
+                System.out.println(posX+" "+posY);
+                if(posX>80&&posX<720){
                     switch(e.getKeyCode()){
                         case 38:posY-=4;break;
                         case 40:posY+=4;break;
@@ -55,8 +63,8 @@ public class Movimentacao extends Frame{
                         default:break;
                     }
                 }
+                lbdeath.setBounds(posX, posY, 32, 48);
                 System.out.println(posX+" "+posY);
-                atualizar();
                 System.out.println(e.getKeyCode());
             }
             @Override
@@ -64,8 +72,5 @@ public class Movimentacao extends Frame{
                 lbdeath.setIcon(walkRight1);
             }
         });
-    }
-    public void atualizar(){
-        lbdeath.setBounds(posX, posY, 32, 48);
     }
 }
