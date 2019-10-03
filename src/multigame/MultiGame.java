6428b687da9d47e5bf48e14b026dd17d334e7c55
edf10 <eduarda.mirelly@academico.ifrn.edu.br>
2019-10-02 21:42:47 -0300

    Início do Jogo Damas

--- src/multigame/MultiGame.java
+++ src/multigame/MultiGame.java
@@ -2,7 +2,7 @@ package multigame;
 import game.Movimentacao;
 public class MultiGame {
     public static void main(String []args){
-        //MultiGameTela mg = new MultiGameTela(1);
-        Movimentacao m = new Movimentacao();
+        MultiGameTela mg = new MultiGameTela(1);
+        //Movimentacao m = new Movimentacao();
     }
 }
