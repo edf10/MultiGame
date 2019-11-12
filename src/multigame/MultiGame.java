dcdc48e16595eb4087ce8d6b918c658352415e84
edf10 <eduarda.mirelly@academico.ifrn.edu.br>
2019-11-12 20:03:16 -0300

    Correção design TelaCM

--- src/multigame/MultiGame.java
+++ src/multigame/MultiGame.java
@@ -2,10 +2,10 @@ package multigame;
 import wordpuzzle.IntroductionWP;
 public class MultiGame {
     public static void main(String []args){
-        //MultiGameTela mg = new MultiGameTela();
-        //mg.intro();
-        //mg.show();
-        IntroductionWP i = new IntroductionWP();
-        i.intro(); i.show();
+        MultiGameTela mg = new MultiGameTela();
+        mg.intro();
+        mg.show();
+        //IntroductionWP i = new IntroductionWP();
+        //i.intro(); i.show();
     }
 }
