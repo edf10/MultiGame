c408d6a467f726b11d12c43819698b39c36605d4
edf10 <50642233+edf10@users.noreply.github.com>
2019-12-03 21:16:23 -0300

    Initial EasterEgg

--- src/multigame/MultiGame.java
+++ src/multigame/MultiGame.java
@@ -1,10 +1,14 @@
 package multigame;
 
+import padroes.EasterEgg;
+
 public class MultiGame {
     public static void main(String []args){
-        MultiGameTela mg = new MultiGameTela();
+        /*MultiGameTela mg = new MultiGameTela();
         mg.intro();
-        mg.show();
-        
+        mg.show();*/
+        EasterEgg e = new EasterEgg();
+        e.addEasterCM();
+        e.show();
     }
 }
